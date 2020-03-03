e7000de8ec5eeb44cc11003d7a4fa2057f8533d1
Bahadır Akın Akgül <akinakgul@gmail.com>
2020-03-04 00:50:02 +0300

    Initial commit

---
+++ src/test/java/com/akgul/starbux/StarbuxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akgul.starbux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarbuxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
